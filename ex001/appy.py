db4c4a708991be5daddfbc731243de3c206f760d
Your Name3 <you@example.com>
2022-12-19 17:37:52 -0300

    Perguntando para o usuario.

---
+++ ex001/appy.py
@@ -0,0 +1,2 @@
+name = input('Input your name: ')
+print('Seja muinto bem vindo, {}'.format(name))
--- tkinter/app.py
+++ tkinter/app.py
@@ -4,5 +4,7 @@ window = Tk()
 window.title('Oliveira.js')
 window.geometry("500x300")
 window.configure(background='#0f0')
+txt = Label(window, text='Texo de teste Oliveira.js', background='#f00', foreground='#00f')
+txt.pack()
 
 window.mainloop()
